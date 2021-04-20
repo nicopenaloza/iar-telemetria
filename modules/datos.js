3b9ec9717ce60b32da2028c7e5a9d06d6db9492a
Nico <nicopenaloza@gmail.com>
2021-04-20 20:48:07 -0300

    Final final

--- modules/datos.js
+++ modules/datos.js
@@ -28,15 +28,15 @@ function GetData(){
    
     let lineaSatelite;
     let index = 0;
-    while (lineaSatelite = Satelite.next()){
+    while (lineaSatelite = Satelite.next()){ // si no va con un solo = no anda, no se porque
 
-        if (index > 1){
+        if (index > 1){ // omite las primeras 2 lineas
 
-            if (index <= config.satelite.nucleos + 1){
+            if (index <= config.satelite.nucleos + 1){ // Temperaturas
                 data.satelite.temperaturas.push(lineaSatelite.toString().replaceAll('\r', ''));
             }
             
-            if (index > config.satelite.nucleos + 1){
+            if (index > config.satelite.nucleos + 1){ // Voltaje
                 data.satelite.voltajes.push(lineaSatelite.toString().replaceAll('\r', ''));
             }
 
@@ -51,20 +51,20 @@ function GetData(){
 
     while (LineaPc = PC.next()){
         
-        const valores = LineaPc.toString().split(' ');
-        if (valores[0] == 'cpu_fan:'){
+        const valores = LineaPc.toString().split(' '); // Crea una lista con todos los valores de la linea
+        if (valores[0] == 'cpu_fan:'){ // Devuelve el RPM del CPU FAN
             data.pc.cpu_fan = parseInt(valores[8]);
         }
 
-        if (valores[0] == "temp1:" || valores[0] == "temp2:"){
+        if (valores[0] == "temp1:" || valores[0] == "temp2:"){ //  Temperaturas 1 y 2 del MOBO
             data.pc.temperaturas.mother.push(valores[8].replaceAll('Â°C', '').replaceAll('+', ''));
         }
 
-        if (valores[0] == "Core"){
+        if (valores[0] == "Core"){ // Temperaturas de los nucleos
             data.pc.temperaturas.cores.push(valores[9].replaceAll('Â°C', '').replaceAll('+', ''));
         }
 
-        if (valores[0] == "Package"){
+        if (valores[0] == "Package"){ // No se de que es pero estaba en los valores del archivo.
             data.pc.temperaturas.cores.push(valores[4].replaceAll('Â°C', '').replaceAll('+', ''));
         }
 
@@ -75,11 +75,11 @@ function GetData(){
     
     while (lineaDisco = Discos.next()){
         
-        if (lineaDisco.toString()[0] == '/') {
+        if (lineaDisco.toString()[0] == '/') { // Busca los discos
             
             const disco = lineaDisco.toString().split(' ');
             
-            disco.forEach(bloque => {
+            disco.forEach(bloque => { // por cada disco busca el porcentaje usado y lo añade a la data Obj
                 
                 if (bloque[2] == '%' || bloque[3] == '%'){
                     data.discos.push({
@@ -94,7 +94,7 @@ function GetData(){
 
 
     }
-    return data;
+    return data; // Devuelve el data object
 
 }
 
