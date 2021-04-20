85ec3005c1074db1ea2a0b3efd40ebcc5d872e63
Nico <nicopenaloza@gmail.com>
2021-04-20 13:53:59 -0300

    Casi finalizado
    Falta solo que devuelva los datos de la pc

--- modules/datos.js
+++ modules/datos.js
@@ -1,10 +1,87 @@
-const fs = require("fs");
+const LineByLine = require('n-readlines');
+const database = require("mime-db");
+const config = require("../config.json");
+
+function GetData(){
+
+    let Satelite = new LineByLine(config.rutas[0]);
+    let PC = new LineByLine(config.rutas[1]);
+    let Discos = new LineByLine(config.rutas[2]);
+
+    let data = {
+        satelite : {
+            temperaturas : [],
+            voltajes : []
+        },
+
+        pc : {
+            temperaturas : {
+                cores : [],
+                mother : []
+            }
+        },
+
+        discos : []
+
+    }
+
+   
+    let lineaSatelite;
+    let index = 0;
+    while (lineaSatelite = Satelite.next()){
+
+        if (index > 1){
+
+            if (index <= config.satelite.nucleos + 1){
+                data.satelite.temperaturas.push(lineaSatelite.toString().replaceAll('\r', ''));
+            }
+            
+            if (index > config.satelite.nucleos + 1){
+                data.satelite.voltajes.push(lineaSatelite.toString().replaceAll('\r', ''));
+            }
+
+        }
+
+        index += 1;
+
+    }
+
+ /*
+    let LineaPc;
+    while (LineaPc = PC.next()){
+         console.log(LineaPc.toString());
+    }
+*/
+
+    let lineaDisco;
+    
+    while (lineaDisco = Discos.next()){
+        
+        if (lineaDisco.toString()[0] == '/') {
+            
+            const disco = lineaDisco.toString().split(' ');
+            
+            disco.forEach(bloque => {
+                
+                if (bloque[2] == '%' || bloque[3] == '%'){
+                    data.discos.push({
+                        name: disco[0],
+                        use: bloque
+                    });
+                }
+
+            });
+
+        }
+
+
+    }
+    return data;
+
+}
 
 module.exports = {
     getInfo: function(){
-        return {
-            temp : 2,
-            test : 2
-        }
+        return GetData();
     }
 }
