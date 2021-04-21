1e2dba68d074076786efcac3e8cb6e7d474dad64
Nicolas Peñaloza <57502001+nicopenaloza@users.noreply.github.com>
2021-04-21 18:31:57 -0300

    Documentación añadida

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const datos = require('./modules/datos.js');
 
 const app = express();
 
-app.set('port', process.env.port || 3000);
+app.set('port', 3000); // Establece que va a trabajar sobre el puerto 3000
 
-app.listen(app.get('port'), () => console.log('Server on port', app.get('port')));
+app.listen(app.get('port'), () => console.log('Server on port', app.get('port'))); // Comienza a escuchar los eventos en este puerto
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res) => { // cuando alguien entre al puerto 3000 con le route /info le va a devolver un json con los datos del equipo 'localhost:3000/info'
     res.send(datos.getInfo());
 });
   
