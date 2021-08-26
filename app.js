26c8b791b455ecd583a2369aca535b2795e0d0e0
Nico <nicopenaloza@gmail.com>
2021-08-26 11:14:40 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const datos = require('./modules/datos.js');
+const config = require('./config');
 
 const app = express();
 
-app.set('port', 3000);
+app.set('port', config.puerto);
 
 app.listen(app.get('port'), () => console.log('Server on port', app.get('port'))); // Comienza a escuchar los eventos en este puerto
 
-app.get('/info', (req, res) => { // cuando alguien entre al puerto 3000 con le route /info le va a devolver un json con los datos del equipo 'localhost:3000/info'
+app.get('/info', (req, res) => { // cuando alguien entre al puerto configurado con le route /info le va a devolver un json con los datos del equipo 'localhost:3000/info'
     res.send(datos.getInfo());
 });
   
