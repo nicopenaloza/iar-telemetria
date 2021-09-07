1bf9afc916ab2c38a01ce78a2f6faafc552e81ce
Nicolas Peñaloza <57502001+nicopenaloza@users.noreply.github.com>
2021-09-07 09:27:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.set('port', config.puerto);
 
-app.listen(app.get('port'), () => console.log('Server on port', app.get('port'))); // Comienza a escuchar los eventos en este puerto
+app.listen(app.get('port'), () => console.log('Servidor iniciado en el puerto', app.get('port'))); // Comienza a escuchar los eventos en este puerto
 
 app.get('/info', (req, res) => { // cuando alguien entre al puerto configurado con le route /info le va a devolver un json con los datos del equipo 'localhost:3000/info'
     res.send(datos.getInfo());
